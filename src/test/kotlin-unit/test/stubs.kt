b885b7ccaff7c6193a0ffa001121824d1d672c52
holi <32414154@qq.com>
2017-08-16 16:34:33 +0800

    fix proxy function

--- src/test/kotlin-unit/test/stubs.kt
+++ src/test/kotlin-unit/test/stubs.kt
@@ -7,6 +7,6 @@ import kotlin.reflect.KClass
 inline fun <reified T : Any> ignored(): T = proxy(T::class) { _, _, _ -> }
 inline fun <reified T : Any> unused(): T = proxy(T::class) { _, _, _ -> throw IllegalStateException() }
 
-@PublishedApi internal fun <T : Any> proxy(type: KClass<T>, handler: (Any, Method, Array<Any>) -> Any) = type.java.let {
+@PublishedApi internal fun <T : Any> proxy(type: KClass<T>, handler: (proxy: Any, method: Method, args: Array<Any>?) -> Any) = type.java.let {
     it.cast(Proxy.newProxyInstance(it.classLoader, arrayOf(it), handler))
 }
