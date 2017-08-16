6bb9c299e2c90b61236ab8c786cafb58192e827f
holi <32414154@qq.com>
2017-08-16 11:06:19 +0800

    make parameter to private

--- src/main/kotlin/auctionsniper/xmpp/XMPPAuctionHouse.kt
+++ src/main/kotlin/auctionsniper/xmpp/XMPPAuctionHouse.kt
@@ -2,10 +2,11 @@ package auctionsniper.xmpp
 
 import auctionsniper.AuctionHouse
 import org.jivesoftware.smack.XMPPConnection
+
 internal const val AUCTION_RESOURCE = "Auction"
 internal const val ITEM_ID_AS_LOGIN = "auction-%s"
 
-class XMPPAuctionHouse private constructor(val connection: XMPPConnection) : AuctionHouse {
+class XMPPAuctionHouse private constructor(private val connection: XMPPConnection) : AuctionHouse {
     companion object {
         fun connect(hostname: String, sniper: String, password: String) = XMPPAuctionHouse(XMPPConnection(hostname).apply {
             connect();login(sniper, password, AUCTION_RESOURCE)
